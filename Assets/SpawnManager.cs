2a3623720b59c4762f95f61e9031a3803f6ffdb0
devanpverma <devanpverma@gmail.com>
2025-10-19 13:24:57 -0400

    Add:SpawnManager

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPostion = new Vector3(25, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPostion, obstaclePrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
