a5a924e5550d2c48e038ff322fb2c1797d643bd4
devanpverma <devanpverma@gmail.com>
2025-11-02 13:18:27 -0500

    Input:GameOverInMoveLeft

--- Assets/MoveLeft.cs
+++ Assets/MoveLeft.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 20;
+    private PlayerController playerController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerController.gameOver== false)
+        {
+             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+       
     }
 }
